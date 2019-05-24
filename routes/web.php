0163305d95b12b98078b661e8a4a96e8ed5e917f
Randol <temastian.jal@gmail.com>
2019-05-23 21:59:01 -0500

    a

--- routes/web.php
+++ routes/web.php
@@ -87,7 +87,7 @@ Route::middleware(['auth'])->group(function(){
 
   Route::get('grupos/{grupo}/pdf', 'GrupoController@pdf')->name('grupos.pdf');
 
-  Route::get('grupos/pdfalumno', 'GrupoController@pdfalumno')->name('grupos.pdfalumno');
+  Route::get('grupos/genere/pdfalumno', 'GrupoController@pdfalumno')->name('grupos.pdfalumno');
 
   Route::get('grupos/{grupo}/pdfin', 'GrupoController@pdfin')->name('grupos.pdfin');
 
